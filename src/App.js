485859f0da6ab58323c966a91b7e5b903c053a99
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-22 11:51:57 +0530

    static list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import { useState } from "react";
 
 function App() {
-  let title = "Counter Application";
-
-  let counter1 = 0;
-  let [counter, setCOunter] = useState(0);
-
-  let str1 = "hello";
-  let [str, setStr] = useState("Hello");
-
-  let list1 = [];
-  let [list, setList] = useState([]);
+  let list = ["delhi", "caluctta"];
 
   return (
     <div>
-      <h1>{title}</h1>
+      <h1>List demo</h1>
+
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
 
       <input type="button" value="Add More Element!" />
     </div>
