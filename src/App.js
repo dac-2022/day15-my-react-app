52858259b6c31264058705d6af862a6e7032213f
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-22 11:56:52 +0530

    list.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { useState } from "react";
 
 function App() {
-  let list = ["delhi", "caluctta"];
+  let [list, setList] = useState(["delhi", "caluctta"]);
+
+  let addNewCity = () => {
+    let value = document.querySelector("#id1").value;
+    // let newlist = ["KOCHI", ...list];
+    let newlist = [value, ...list];
+    setList(newlist);
+  };
 
   return (
     <div>
       <h1>List demo</h1>
 
+      <input type="text" id="id1" />
+      <input type="button" value="Add More Element!" onClick={addNewCity} />
+
       {list.map((item) => (
         <h1>{item}</h1>
       ))}
-
-      <input type="button" value="Add More Element!" />
     </div>
   );
 }
