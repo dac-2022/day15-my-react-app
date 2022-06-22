71ebe0055c00a15d8b2497d9b738098e806abd16
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-22 10:47:54 +0530

    b5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 function App() {
-  let price = 100;
   let str = "CDAC";
-  let ref = { name: "rahul" };
-  let myfun = (a, b) => a + b;
+  let myfun = () => {
+    alert();
+    return "Helo";
+  };
 
   return (
     <div>
       <h1>{str}</h1>
-      <h1>{price}</h1>
-      <h1>{ref.name}</h1>
-      <h1>{myfun(1, 1)}</h1>
 
-      <h1 id="id1">{str}</h1>
-      <h1 id={price}> {str}</h1>
+      <input type="button" value="Click Me!" onClick={myfun} />
     </div>
   );
 }
