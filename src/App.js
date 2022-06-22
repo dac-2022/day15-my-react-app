6f6adc83f3fe74a079a98e78d59e099348a8be3d
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-22 11:01:38 +0530

    b7

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { useState } from "react";
+
 function App() {
-  let title = "Counter Application";
-  let counter = 1;
+  let [title] = useState("Counter Application");
+  let [counter, setCounter] = useState(1);
 
   let likeMe = () => {
     counter += 1;
-    console.log(counter);
+    setCounter(counter);
   };
 
   return (
     <div>
       <h1>{title}</h1>
+      <h1>{counter}</h1>
+      <h1>{counter}</h1>
       <h1>{counter}</h1>
 
       <input type="button" value="Like Me" onClick={likeMe} />
